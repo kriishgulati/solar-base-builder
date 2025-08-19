a8483b2a97e2e7863348185a73f7804f4ccbb17a
Solabrix <n6756504@gmail.com>
2025-08-19 16:14:20 +0530

    test commit

--- src/components/SimpleCanvas.tsx
+++ src/components/SimpleCanvas.tsx
@@ -16,6 +16,7 @@ export const SimpleCanvas = ({ setShowObstacleMode }: { setShowObstacleMode: (v:
   const [groupDragOffset, setGroupDragOffset] = useState<{x: number, y: number}>({x: 0, y: 0});
 
 //hello test
+//bye test
   const {
     shapes,
     selectedShapeId,
